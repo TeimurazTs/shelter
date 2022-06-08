05963ef20012cf4940bfb66bb335f464cc7c47ed
TeimurazTs <temotsuladze99@gmail.com>
2022-06-08 14:55:23 +0400

    additional changes

--- pages/pets/script.js
+++ pages/pets/script.js
@@ -33,10 +33,12 @@ let domDoubleMinus = document.querySelector(".dom-double-minus");
 
 domPaginationMinus.addEventListener("click", () => {
   if (counter < 2) {
-    domPaginationMinus.removeEventListener();
+    domPaginationMinus.removeEventListener("click");
   }
   counter--;
   domPaginationCount.textContent = counter;
+  domDoubleArrowPlus.addEventListener("click", doublePlus);
+  domDoubleMinus.addEventListener("click", minusFunction);
   fetch("../Pets.json")
     .then((response) => {
       return response.json();
@@ -80,9 +82,11 @@ function pagination(petsObject) {
   createErray();
   createErray2();
   counter++;
+  domDoubleArrowPlus.addEventListener("click", doublePlus);
+  domDoubleMinus.addEventListener("click", minusFunction);
   if (counter > 6) {
     counter = 6;
-    domPointerButton.removeEventListener();
+    domPointerButton.removeEventListener("click");
     evenListenerCout = 0;
   }
   domPaginationCount.textContent = counter;
@@ -95,32 +99,16 @@ function pagination(petsObject) {
   }
 }
 
-domDoubleArrowPlus.addEventListener("click", () => {
+domDoubleArrowPlus.addEventListener("click", doublePlus);
+
+function doublePlus() {
   counter = 6;
   domPaginationCount.textContent = counter;
-  if (counter > 6) {
-    domDoubleArrowPlus.removeEventListener();
-  }
-  fetch("../Pets.json")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      createErray();
-      createErray2();
-      for (let i = 0; i < 8; i++) {
-        domNotPets[i].children[0].setAttribute(
-          "src",
-          `${data[thisarray[i]].img}`
-        );
-        domNotPets[i].children[1].textContent = data[thatarray[i]].name;
-      }
-    })
-});
-
-domDoubleMinus.addEventListener("click", () => {
-  counter = 1;
-  domPaginationCount.textContent = counter;
+  domDoubleMinus.addEventListener("click", minusFunction);
+  setTimeout(() => {
+    console.log("ji");
+    domDoubleArrowPlus.removeEventListener("click", doublePlus);
+  }, 0);
   fetch("../Pets.json")
     .then((response) => {
       return response.json();
@@ -136,4 +124,31 @@ domDoubleMinus.addEventListener("click", () => {
         domNotPets[i].children[1].textContent = data[thatarray[i]].name;
       }
     });
-});
+}
+
+domDoubleMinus.addEventListener("click", minusFunction);
+
+function minusFunction() {
+  counter = 1;
+  domPaginationCount.textContent = counter;
+  domDoubleArrowPlus.addEventListener("click", doublePlus);
+  setTimeout(() => {
+    console.log("ji");
+    domDoubleMinus.removeEventListener("click", minusFunction);
+  }, 0);
+  fetch("../Pets.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      createErray();
+      createErray2();
+      for (let i = 0; i < 8; i++) {
+        domNotPets[i].children[0].setAttribute(
+          "src",
+          `${data[thisarray[i]].img}`
+        );
+        domNotPets[i].children[1].textContent = data[thatarray[i]].name;
+      }
+    });
+}
